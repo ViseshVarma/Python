99c462fa6cfa13cc0e940b77920dd7adce3322ee
Visesh Varma <40001042+ViseshVarma@users.noreply.github.com>
2023-03-26 19:31:12 +0530

    26-Mar-2023

---
+++ 100 Days of Code - Angela Yu/Day_1 (Band_name_Generator).py
@@ -0,0 +1,9 @@
+# code for Band name generator
+
+print('Hi, welcome to Band name generator')
+
+city = input('Which City did you grow up in ?\n')
+
+pet = input('What is the name of your pet\n')
+
+print("the name of your band name is: \n", city + pet)
