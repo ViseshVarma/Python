b3431edb1298b6016d5fe26cb3a53be0160380a2
Visesh Varma <40001042+ViseshVarma@users.noreply.github.com>
2023-05-07 16:56:23 +0530

    07-05-2023

--- 100 Days of Code - Angela Yu/Day_10.py
+++ 100 Days of Code - Angela Yu/Day_10.py
@@ -1,36 +1,46 @@
 # calculator
 
-# add
+from art import logo
+
 def add(n1, n2):
-    return n1 + n2
+  return n1 + n2
 
-# sub
-def sub(n1, n2):
-    return n1 - n2
+def subtract(n1, n2):
+  return n1 - n2
 
-# multiply
-def mul(n1, n2):
-    return n1 * n2
+def multiply(n1, n2):
+  return n1 * n2
 
-#div
-def div(n1, n2):
-    return n1 / n2
+def divide(n1, n2):
+  return n1 / n2
 
 operations = {
-    "+": add,
-    "-": sub,
-    "*": mul,
-    "/": div
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide
 }
 
-num1 = int(intput("what's the first number: "))
-for symbol in operations:
+def calculator():
+  print(logo)
+  num1 = float(input("What's the first number?: "))
+  for symbol in operations:
     print(symbol)
-operation_symbol = input("Pick an operation from the line above: ")
-num2 = int(input("What's the second number"))
-calculation_function = operations[operation_symbol]
-answer = calculation_function(num1, num2)
+  should_continue = True
+ 
+  while should_continue:
+    operation_symbol = input("Pick an operation: ")
+    num2 = float(input("What's the next number?: "))
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+      num1 = answer
+    else:
+      should_continue = False
+      calculator()
+
+calculator()
 
     
