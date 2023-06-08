fd7be7d192fe830ec5ead0b5c121a6cf08d91183
Visesh Varma <40001042+ViseshVarma@users.noreply.github.com>
2023-06-08 21:31:54 +0530

    05-June-2023

--- 100 Days of Code - Angela Yu/Day_20/main.py
+++ 100 Days of Code - Angela Yu/Day_20/main.py
@@ -1,18 +1,45 @@
-from turtle import Screen
-from Snake_Game import Snake
-import time
+from turtle import Turtle
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
-screen = Screen()
-screen.setup(600, 600)
-screen.bgcolor("black")
-screen.title("My Snake Game")
-screen.tracer(0)
 
-snake = Snake()
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
 
-game_is_on = True
-while game_is_on:
-    screen.update()
-    time.sleep(0.1)
+    def create_snake(self):
+        for positions in STARTING_POSITIONS:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(positions)
+            self.segments.append(new_segment)
 
-    snake.move()
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
