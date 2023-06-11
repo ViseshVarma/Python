fb7e7d4a6e80546f04c775686b87b38bee41fd4b
Visesh Varma <40001042+ViseshVarma@users.noreply.github.com>
2023-06-11 12:01:34 +0530

    11-June-2023

--- 100 Days of Code - Angela Yu/Day_21/Pong_game.py
+++ 100 Days of Code - Angela Yu/Day_21/Pong_game.py
@@ -1,5 +1,7 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 
@@ -12,6 +14,7 @@ screen.tracer(0)
 # creating paddles
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
 
 # Listening to key input from user
 screen.listen()
@@ -25,7 +28,27 @@ screen.onkey(l_paddle.go_down, "s")
 # if the below code is not written then the paddle will not show up.
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+
+    # Detect collision with the wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        # needs to bounce off the top wall
+        ball.bounce_y()
+
+    # Detect collision with r_paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # Detect r_paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    # Detect l_paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+
 
 # To exit from the game when any part of the window is clicked
 screen.exitonclick()
