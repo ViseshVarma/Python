a8cdcba7667a51599b7594055c3f015ebd59ea7b
Visesh Varma <40001042+ViseshVarma@users.noreply.github.com>
2023-06-26 23:41:50 +0530

    26-June-2023

---
+++ 100 Days of Code - Angela Yu/Day_25/US_states_game/main.py
@@ -0,0 +1,22 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+
+
+answer_state = screen.textinput(title="Guess the state", prompt = "What's another state's name ? ")
+
+if answer_state in all_states:
+    t = turtle.Turtle()
+    t.hideturtle()
+    t.penup()
+    state_data = data[data.state == answer_state]
+    t.goto(state_data.x, state_data.y)
+    t.write(state_data.state)
