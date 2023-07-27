ae734fceb735690494a4bf24ff3d4a0b8b099bfd
Visesh Varma <40001042+ViseshVarma@users.noreply.github.com>
2023-07-27 23:31:25 +0530

    27-July-2023

--- 100 Days of Code - Angela Yu/Day_29/Password_Manager.py
+++ 100 Days of Code - Angela Yu/Day_29/Password_Manager.py
@@ -4,14 +4,48 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+def save():
+
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    with open("data.txt", "a") as data_file:
+        data_file.write(f"{website} | {email} | {password}")
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
 window.config(padx=20, pady=20)
 
-canvas = Canvas(width=400, height=400)
+canvas = Canvas(width=200, height=200)
 logo = PhotoImage(file='logo.png')
-canvas.create_image(200, 200, image=logo)
-canvas.pack()
+canvas.create_image(100, 100, image=logo)
+canvas.grid(row=0, column=1)
+
+# Labels
+website_label = Label(text="Website: ")
+website_label.grid(row=1, column=0)
+email_label = Label(text="Email/Username: ")
+email_label.grid(row=2, column=0)
+Password_label = Label(text="Password: ")
+Password_label.grid(row=3, column=0)
+
+# Entries
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
+email_entry = Entry(width=35)
+email_entry.grid(row=2,column=1, columnspan=2)
+email_entry.insert(0, "abc@gmail.com")
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
+
+# Buttons
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(row=3, column=2)
+add_button = Button(text="Add", width=36, command=save)
+add_button.grid(row=4, column=1,columnspan=2)
 
 window.mainloop()
